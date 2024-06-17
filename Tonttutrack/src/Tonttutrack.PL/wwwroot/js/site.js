f6c5dc9e49976210f90978bbb33bddde3adeca6b
Ivan Dadakov <85338820+IIDadakov20@users.noreply.github.com>
2024-06-17 22:04:03 +0300

    Implement device connection and real-time data fetching functionality.

--- Tonttutrack/src/Tonttutrack.PL/wwwroot/js/site.js
+++ Tonttutrack/src/Tonttutrack.PL/wwwroot/js/site.js
@@ -1,4 +1,42 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function () {
+    $('#deviceConnectionForm').on('submit', function (e) {
+        e.preventDefault();
 
-// Write your JavaScript code.
+        var formData = {
+            Code: $('#Code').val(),
+            Password: $('#Password').val()
+        };
+
+        // Send AJAX request
+        $.ajax({
+            url: '/trackerDevice/connectDevice',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(formData),
+            success: function (response) {
+                if (response === true) {
+                    readRoutePoints();
+                }
+            },
+            error: function (xhr, status, error) {
+                console.error(xhr.responseJSON.message);
+            }
+        });
+    });
+});
+
+function readRoutePoints() {
+    setInterval(function () {
+        $.ajax({
+            url: '/trackerDevice/readRoutePoint',
+            type: 'GET',
+            contentType: 'application/json',
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (xhr, status, error) {
+                console.error(xhr.responseJSON.message);
+            }
+        });
+    }, 5000);
+}
