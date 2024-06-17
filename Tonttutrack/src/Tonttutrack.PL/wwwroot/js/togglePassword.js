9e60e5ba3092640f8d38ce53dac626edc0140565
Martin Kafedzhiev <mikafedzhiev20@codingburgas.bg>
2024-06-17 18:48:38 +0300

    Add password visibility toggle. Close #13

---
+++ Tonttutrack/src/Tonttutrack.PL/wwwroot/js/togglePassword.js
@@ -0,0 +1,12 @@
+document.getElementById('togglePassword').addEventListener('change', function() {
+    var passwordInput = document.getElementById('passwordInput');
+    var confirmPasswordInput = document.getElementById('confirmPasswordInput');
+    
+    if (this.checked) {
+        passwordInput.type = 'text';
+        confirmPasswordInput.type = 'text';
+    } else {
+        passwordInput.type = 'password';
+        confirmPasswordInput.type = 'password';
+    }
+});
