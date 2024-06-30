9f3fd501374d1a0b8f99102347a3bbac1ed1f9fd
Ivan Dadakov <85338820+IIDadakov20@users.noreply.github.com>
2024-06-30 19:26:51 +0300

    Fix design issues.

--- Tonttutrack/src/Tonttutrack.PL/wwwroot/js/togglePassword.js
+++ Tonttutrack/src/Tonttutrack.PL/wwwroot/js/togglePassword.js
@@ -1,13 +1,22 @@
 document.getElementById('togglePassword').addEventListener('change', function() {
     var passwordInput = document.getElementById('passwordInput');
     var confirmPasswordInput = document.getElementById('confirmPasswordInput');
-    
-    if (this.checked) {
-        passwordInput.type = 'text';
-        confirmPasswordInput.type = 'text';
-    } else {
-        passwordInput.type = 'password';
-        confirmPasswordInput.type = 'password';
+
+    if (window.location.pathname === "/Authentication/Register") {
+        if (this.checked) {
+            passwordInput.type = 'text';
+            confirmPasswordInput.type = 'text';
+        } else {
+            passwordInput.type = 'password';
+            confirmPasswordInput.type = 'password';
+        }
+    }
+    else {
+        if (this.checked) {
+            passwordInput.type = 'text';
+        } else {
+            passwordInput.type = 'password';
+        }
     }
 });
 document.getElementById('togglePassword').onmousedown = function (event) {
